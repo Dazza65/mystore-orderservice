7b26d34424b2825b73914fe2963bd13b7013c3c5
Darren Harris <dazza@me.com>
2020-10-14 20:04:23 +0800

    Return JSON from health status check. Add call to AWS SSM parameter store to simulate DB failure

--- src/service.js
+++ src/service.js
@@ -1,28 +1,43 @@
 const express  = require("express");
+const awsParamStore = require( 'aws-param-store' );
 const app = express(); 
 const service = "orders"
 const port = 8080;
 
 const orders = [
-  { id: 1, customerId: 1, item: "Widget1" },
-  { id: 2, customerId: 1, item: "Widget2" },
-  { id: 3, customerId: 2, item: "Widget3" },
+  { id: 1, customerId: 1, orderDate: "Widget1" },
+  { id: 2, customerId: 1, orderDate: "Widget2" },
+  { id: 3, customerId: 2, orderDate: "Widget3" },
 ];
 
 let orderList = [];
 
 app.get(`/${service}/`, function(req,res){
   console.log("Get customer with ID: " + req.query.customerID);
-  orderList = orders.filter( order => { 
-    return (order.customerId === parseInt(req.query.customerID));
-  });
 
-   res.send(orderList); 
+  awsParamStore.getParameter( '/mystore/simulate-db-failure', { region: 'ap-southeast-2' } )
+    .then( (simulateDBFailure) => {
+
+    console.log("simulateDBFailre: " + simulateDBFailure.Value);
+
+    if ( simulateDBFailure.Value === 'True' ) {
+      let err = { "Error:" : "Failed to get customer orders. Simulated DB failure." };
+      console.log(err);
+      res.status(503).send(JSON.stringify(err));
+
+    } else {
+      orderList = orders.filter( order => { 
+        return (order.customerId === parseInt(req.query.customerID));
+      });
+
+      res.send(orderList);    
+    }
+   
+  });
 });
 
 app.get(`/${service}/status`, function(req,res){
-  console.log("Status check...");
-  res.send("{OK}"); 
+  res.send("{\"Status\": \"OK\"}");
 });
 
 app.listen(port, function (){
